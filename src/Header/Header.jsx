0c5538e2835f277ce2d1e889fd2e536c4784efd0
scartisp <simion.cartis1@gmail.com>
2026-08-25 20:07:11 -0400

    added link to skills section

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
                 <a href='#home' className="logo a-link">Simion Cartis</a>
                 <ul className='header-list-element header-nav'>
                     <li> <a href='#about' className='a-link'>About</a></li>
-                    <li>Skills</li>
+                    <li> <a href='#skills' className='a-link'> Skills </a></li>
                     <li>Experience</li>
                     <li>Portfolio</li>
                     <li>Contact</li>

2b12c4d76935068fccaa148873d9582159f8adc1
scartisp <simion.cartis1@gmail.com>
2026-09-02 13:21:25 -0400

    corrected grammatical error in about

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
             <div className='about-content'>
                 <div className='about-text'>
                     <p>With an insatiable appetite for learning, I thrive where innovation blooms. Always looking for the next hill to climb, I am constantly learning and refining skills. Be it android development, web design, machine learning, or new guitar chops, you will always find me with my nose to the grind stone.</p>
-                    <p>Outside of programming, I enjoy playing playing and listening to music, going to the gym, and reading. My current favorite band is Opeth and my current read is Frankenstein</p>
+                    <p>Outside of programming, I enjoy playing and listening to music, going to the gym, and reading. My current favorite band is Opeth and my current read is Frankenstein</p>
                 </div>
                     <img src={coding} className='code-img'></img>
             </div>
